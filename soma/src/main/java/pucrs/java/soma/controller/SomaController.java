62f8dd0b738e3d93e80e9afd636c3ed945336600
Marco Mangan <marco.mangan@gmail.com>
2019-06-14 17:24:21 -0300

    #3 #6

--- soma/src/main/java/pucrs/java/soma/controller/SomaController.java
+++ soma/src/main/java/pucrs/java/soma/controller/SomaController.java
@@ -2,13 +2,19 @@ package pucrs.java.soma.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
   
 @Controller
 public class SomaController {
 
-	@RequestMapping("/add")
+	@GetMapping("/add")
+	private String somar() {
+		return "formSoma";
+	}
+	
+	@PostMapping("/add")
 	private String somar(@RequestParam int a, 
 			@RequestParam int b, 
 			Model model) {
